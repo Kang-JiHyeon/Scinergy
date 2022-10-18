fd7f573af4dd320fc7f56d7d49f8ecf8df21152c
Ellian <dbsrn0125@naver.com>
2022-10-18 21:19:41 +0900

    abc

--- Assets/KYG/Scripts/Rotate.cs
+++ Assets/KYG/Scripts/Rotate.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    bool isRotate = false;
     public float rotSpeed = 205;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,10 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed);
+        if(isRotate) transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed);
+    }
+    public void OnRotateBtn()
+    {
+        isRotate = !isRotate;
     }
 }
