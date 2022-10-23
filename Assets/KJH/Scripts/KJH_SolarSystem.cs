d8c66da7e8f3038e35888be889973eb11c874cd8
Kang-JiHyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2022-10-23 18:14:23 +0900

    스크롤바 값에 따른 공전 각도 변경

--- Assets/KJH/Scripts/KJH_SolarSystem.cs
+++ Assets/KJH/Scripts/KJH_SolarSystem.cs
@@ -24,6 +24,7 @@ public class KJH_SolarSystem : MonoBehaviour
 
     // 슬라이더 바에 따른 초당 단위시간 변경
     public Scrollbar unitTimeScrolbar;
+    public Text unitTimeText;
 
     public enum UnitTime
     {
@@ -39,8 +40,9 @@ public class KJH_SolarSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         // 행성 리스트에 추가
-        for (int i=0; i < go_planets.childCount; i++)
+        for (int i = 0; i < go_planets.childCount; i++)
         {
             planets.Add(go_planets.GetChild(i).transform);
             // 행성 자전축 기울기 초기화
@@ -52,7 +54,7 @@ public class KJH_SolarSystem : MonoBehaviour
             // 태양-행성 간의 거리 초기화
             planets[i].localPosition = new Vector3(1, 0, 1) * AUs[i] * 10;
 
-            
+
 
             // 행성 궤도 그리기
             lr = planets[i].GetChild(0).GetComponent<LineRenderer>();
@@ -60,17 +62,16 @@ public class KJH_SolarSystem : MonoBehaviour
 
             DrawOrbit();
         }
-
     }
 
-    
     // 0.02초 마다 update => 1초에 50번 호출
     private void FixedUpdate()
     {
-        for(int i = 0; i<planets.Count; i++)
+        for (int i = 0; i < planets.Count; i++)
         {
             // 단위 시간 당 공전 주기에 따른 rot angle 설정
-            rotAngle = 360f / yearPeriods[i] / (float)unitTime;
+            //rotAngle = 360f / yearPeriods[i] / (float)unitTime;
+            rotAngle = (360f / yearPeriods[i] / (float)unitTime) * unitTimeNum;
 
             // 각도만큼 회전
             planets[i].RotateAround(transform.position, -transform.up, rotAngle * Time.fixedDeltaTime);
@@ -98,7 +99,7 @@ public class KJH_SolarSystem : MonoBehaviour
         lr.positionCount = segment + 1;
         lr.SetPositions(points);
     }
-    
+
     Vector3 CalculatePosition(float t)
     {
         float angle = Mathf.Deg2Rad * 360f * t;
@@ -107,9 +108,62 @@ public class KJH_SolarSystem : MonoBehaviour
         return new Vector3(x, 0, z);
     }
 
+    int month = 11;
+    int day = 30;
+    int hour = 23;
+    float standard = 0.5f / 65f;
+    float unitTimeNum = 0f;
+    float sign = 0;
+    string unitTimeTxt = "";
+
+    // 스크롤바의 값에 따라 단위시간을 결정하고 싶다.
     public void SetUnitTime()
     {
+        sign = unitTimeScrolbar.value > 0.5f ? 1 : -1;
 
+
+        float value = Mathf.Abs(0.5f - unitTimeScrolbar.value) / standard;
+
+        if (value < 1f)
+        {
+            unitTimeNum = 0;
+            unitTimeText.text = "멈춤";
+        }
+        else
+        {
+            if (value >= hour + day + month)
+            {
+                unitTimeNum = 1;
+                unitTimeTxt = "년";
+
+                unitTime = UnitTime.year;
+
+            }
+            else if (value > hour + day)
+            {
+                unitTimeNum = MathF.Truncate(value - (hour + day) * standard - (hour + day)) % (month + 1) + 1;
+                unitTimeTxt = "월";
+
+                unitTime = UnitTime.month;
+            }
+            else if (value > hour + 1)
+            {
+                unitTimeNum = MathF.Truncate(value - hour * standard - hour) % (day + 1) + 1;
+                unitTimeTxt = "일";
+
+                unitTime = UnitTime.day;
+            }
+            else
+            {
+                unitTimeNum = MathF.Truncate(value % (hour + 1));
+                unitTimeTxt = "시간";
+
+                unitTime = UnitTime.hour;
+
+            }
+            unitTimeNum *= sign;
+            unitTimeText.text = unitTimeNum.ToString("+#;-#;0")+ unitTimeTxt;
+        }
     }
 
 }
