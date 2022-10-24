3b50c08469e72cdff922b624f73ac2063b4f9c52
Ellian <dbsrn0125@naver.com>
2022-10-24 10:29:47 +0900

    abc

--- Assets/KYG/Scripts/WorldMap.cs
+++ Assets/KYG/Scripts/WorldMap.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 
 public class WorldMap  : MonoBehaviour, IPointerClickHandler
 {
+    private GraphicRaycaster GraphicRaycaster;
+    private void Awake()
+    {
+        GraphicRaycaster = GetComponentInChildren<GraphicRaycaster>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +20,29 @@ public class WorldMap  : MonoBehaviour, IPointerClickHandler
     // Update is called once per frame
     void Update()
     {
-        
+        OnGraphicRayCast();
+    }
+    public void OnGraphicRayCast()
+    {
+        PointerEventData ped = new PointerEventData(null);
+        ped.position = Input.mousePosition;
+        List<RaycastResult> results = new List<RaycastResult>();
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GraphicRaycaster.Raycast(ped, results);
+            if (results.Count <= 0) return;
+            //results[0].gameObject.transform.position = ped.position;
+            print(results);
+            print(results[0].gameObject.name);
+            print(Input.mousePosition);
+            print(results[0].gameObject.transform.position);
+        }
     }
     public void OnPointerClick(PointerEventData eventData)
     {
         if(eventData.button == PointerEventData.InputButton.Left)
         {
-            Debug.Log("Mouse Position : " + eventData.position);
+            //Debug.Log("Mouse Position : " + eventData.position);
         }
     }
 }
