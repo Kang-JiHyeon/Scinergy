d2d445d97b983f371b2fb949df0ba131d5376ab3
fianceO <will0094@naver.com>
2022-10-26 14:57:48 +0900

    .

---
+++ Assets/OSW/Scripts/OSW_LineDrawer.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSW_LineDrawer : MonoBehaviour
+{
+    List<Vector3> linePoints;
+    float timer;
+    public float timeDelay;
+    GameObject newLine;
+    LineRenderer drawLine;
+    public float linewidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        linePoints = new List<Vector3>();
+        timer = timeDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            newLine = new GameObject();
+            drawLine = newLine.AddComponent<LineRenderer>();
+            drawLine.material = new Material(Shader.Find("Sprites/Default"));
+            drawLine.startColor = RandomColor();
+            drawLine.endColor = RandomColor();
+            drawLine.startWidth = linewidth;
+            drawLine.endWidth = linewidth;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            timer -= Time.deltaTime;
+            if(timer <= 0)
+            {
+                linePoints.Add(GetMousePosition());
+                drawLine.positionCount = linePoints.Count;
+                drawLine.SetPositions(linePoints.ToArray());
+
+                timer = timeDelay;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            linePoints.Clear();
+        }
+    }
+
+    Vector3 GetMousePosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        return ray.origin + ray.direction * 10;
+    }
+
+    Color RandomColor()
+    {
+        return Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+}
