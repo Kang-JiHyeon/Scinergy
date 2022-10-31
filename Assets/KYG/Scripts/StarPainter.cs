b71d4494aab7c019f905500115ba9909c848b2a7
Ellian <dbsrn0125@naver.com>
2022-10-31 15:43:46 +0900

    스타페인터 스크립트 추가

--- Assets/KYG/Scripts/StarPainter.cs
+++ Assets/KYG/Scripts/StarPainter.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class StarPainter : MonoBehaviour
 {
+    LineRenderer starLine;
+    public Vector3 star1Pos, star2Pos;
+    public GameObject star2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        starLine = GetComponent<LineRenderer>();
+        starLine.startWidth = 1f;
+        starLine.endWidth = 1f;
+        star1Pos = gameObject.GetComponent<Transform>().position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        starLine.SetPosition(0, star1Pos);
+        starLine.SetPosition(1, star2.transform.position);
     }
 }
