70b8d75bdd18e5cfcfceae8aba31777e5afcde91
duddke <sya1008@naver.com>
2022-11-13 00:56:34 +0900

    플레이어 수정 제발 최종이길

--- Assets/SYA/Scripts/SYA_PlayerCompo.cs
+++ Assets/SYA/Scripts/SYA_PlayerCompo.cs
@@ -9,23 +9,20 @@ public class SYA_PlayerCompo : MonoBehaviourPun
    PlayerMove SYA_PlayerMove;
    CharacterController CharacterController;
    PlayerRot PlayerRot;
-    SYA_DontDestroy SYA_DontDestroy;
 
     private void Start()
     {
         SYA_PlayerMove = GetComponent<PlayerMove>();
         CharacterController = GetComponent<CharacterController>();
         PlayerRot=GetComponent<PlayerRot>();
-        SYA_DontDestroy = GetComponent<SYA_DontDestroy>();
     }
     private void Update()
     {
-        bool kjh = SceneManager.GetActiveScene().name.Contains("KJH")||!photonView.IsMine;
+        bool kjh = SceneManager.GetActiveScene().name.Contains("KJH");
         transform.GetChild(0).gameObject.SetActive(!kjh);
         transform.GetChild(1).gameObject.SetActive(!kjh);
         SYA_PlayerMove.enabled = !kjh;
         CharacterController.enabled= !kjh;
         PlayerRot.enabled= !kjh;
-        SYA_DontDestroy.enabled = !kjh;
     }
 }
