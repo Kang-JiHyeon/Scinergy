7b11bee2f7a325ccd9ca934c4cabeaeb2691bd9e
duddke <sya1008@naver.com>
2022-11-22 11:11:32 +0900

    파일로더 구현중 / 플레이어 컴포넌트 관리자 수정

---
+++ Assets/SYA/Scripts/Game_Lobby/SYA_PlayerCompo.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class SYA_PlayerCompo : MonoBehaviourPun
+{
+    public static SYA_PlayerCompo Instance;
+    PlayerMove PlayerMove;
+    CharacterController CharacterController;
+    PlayerRot PlayerRot;
+    SYA_DontDestroy SYA_DontDestroy;
+
+    /*private void Awake()
+    {
+        *//*if (Instance == null)
+        {
+            //인스턴스에 나를 넣고
+            Instance = this;
+            //나를 씬이 전환이 되어도 파괴되지 않게 하겠다
+
+            DontDestroyOnLoad(gameObject);
+        }
+        //그렇지 않으면
+        else
+        {
+            Destroy(gameObject);
+        }*//*
+    }*/
+
+    private void Start()
+    {
+        PlayerMove = GetComponent<PlayerMove>();
+        CharacterController = GetComponent<CharacterController>();
+        PlayerRot = GetComponent<PlayerRot>();
+    }
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name.Contains("KJH"))
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(false);
+            PlayerMove.enabled = false;
+            CharacterController.enabled = false;
+            PlayerRot.enabled = false;
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(1).gameObject.SetActive(true);
+            CharacterController.enabled = true;
+            //채팅을 치는 중이거나 전체화면일 때는 이동 및 카메라 회전이 불가능하다
+            PlayerMove.enabled = !(PlayerMove.fullScreenMode ||
+                (SYA_ChatManager.Instance.gameObject != null ? SYA_ChatManager.Instance.inputFocused : false));
+            PlayerRot.enabled = !(PlayerMove.fullScreenMode ||
+                (SYA_ChatManager.Instance.gameObject != null ? SYA_ChatManager.Instance.inputFocused : false));
+        }
+    }
+}
