d2b34c1eca12f750d485c168768e90df19a43209
Ellian <dbsrn0125@naver.com>
2022-11-24 14:58:22 +0900

    CSV test

--- Assets/KYG/Scripts/DataManager.cs
+++ Assets/KYG/Scripts/DataManager.cs
@@ -44,7 +44,7 @@ public class DataManager : MonoBehaviour
 
     public GameObject starFactory;
 
-    public GameObject starLine;
+    public GameObject starLineFactory;
 
     public List<GameObject> brightnessList = new();
     private void Awake()
@@ -65,31 +65,42 @@ public class DataManager : MonoBehaviour
         zodiacInfo = CSV.instance.Parsing<ZodiacInfo>("Zodiac");
         starInfo = CSV.instance.Parsing<StarInfo>("starInfo");
         starTests = CSV.instance.Parsing<StarTest>("starTest");
-        int starIndex = 0;
-        for(int i = 0; i<zodiacInfo.Count; i++)
-        {
-            GameObject constellation = Instantiate(ConstellationFactory);
-            constellation.transform.parent = GameManager.instance.CelestialSphere.transform;
-            constellation.name = zodiacInfo[i].name;
-            GameManager.instance.createdConstellationList[name] = constellation;
-            for (int j = starIndex; j < zodiacInfo[i].starCount + starIndex; j++)
-            {
-                GameObject star = Instantiate(starFactory);
-                GameManager.instance.createdStarList[starInfo[j].starName] = star;
-                GameObject brightness = brightnessList.Find(x => x.name == starInfo[j].brightness);
-                star.GetComponent<Star>().InfoSet(starInfo[j].starName, starInfo[j].ra, starInfo[j].dec, starFactory, brightness,3, 1);
-                star.transform.parent = constellation.transform;
-            }
-            starIndex+= zodiacInfo[i].starCount;
-            
-        }
-        //for(int i = 0; i<starInfo.Count; i++)
+        //int starIndex = 0;
+        //for(int i = 0; i<zodiacInfo.Count; i++)
         //{
-        //    GameObject star = Instantiate(starFactory);
-        //    GameManager.instance.createdStarList[starInfo[i].starName] = star;
-        //    star.GetComponent<Star>().InfoSet(starInfo[i].starName, starInfo[i].ra, starInfo[i].dec,starFactory, brightness, 1);
+        //    GameObject constellation = Instantiate(ConstellationFactory);
+        //    constellation.transform.parent = GameManager.instance.CelestialSphere.transform;
+        //    constellation.name = zodiacInfo[i].name;
+        //    GameManager.instance.createdConstellationList[name] = constellation;
+        //    for (int j = starIndex; j < zodiacInfo[i].starCount + starIndex; j++)
+        //    {
+        //        GameObject star = Instantiate(starFactory);
+        //        GameManager.instance.createdStarList[starInfo[j].starName] = star;
+        //        GameObject brightness = brightnessList.Find(x => x.name == starInfo[j].brightness);
+        //        star.GetComponent<Star>().InfoSet(starInfo[j].starName, starInfo[j].ra, starInfo[j].dec, starFactory, brightness,3, 1);
+        //        star.transform.parent = constellation.transform;
+        //    }
+        //    starIndex+= zodiacInfo[i].starCount;          
         //}
-    }
+        for(int i = 0; i< starTests.Count; i++)
+        {
+            GameObject star = Instantiate(starFactory);
+            GameManager.instance.createdStarList[starTests[i].name] = star;
+            GameObject brightness = brightnessList.Find(x => x.name == starTests[i].brightness);
+            star.GetComponent<Star>().InfoSet(starTests[i].name, starTests[i].ra, starTests[i].dec, starFactory, brightness, starTests[i].apparentMagnitude, 1);
+            star.transform.parent = GameManager.instance.CelestialSphere.transform;
+        }
+        for(int i = 0; i<starTests.Count; i++)
+        {
+            string[] starLineList = starTests[i].starLine.Split(":");
+            for(int j = 0; j<starLineList.Length; j++)
+            {
+                StarLine starLine = Instantiate(starLineFactory).GetComponent<StarLine>();
+                starLine.star1 = GameObject.Find(starTests[i].name);
+                starLine.star2 = GameObject.Find(starLineList[j]);
+            }
+        }
+    }   
 
     // Update is called once per frame
     void Update()
