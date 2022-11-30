ba854105409aa6844e0e289a2f32a957b0a85c4a
duddke <sya1008@naver.com>
2022-11-30 13:22:55 +0900

    베타 수정

--- Assets/SYA/Scripts/Game_Lobby/SYA_Voice.cs
+++ Assets/SYA/Scripts/Game_Lobby/SYA_Voice.cs
@@ -10,6 +10,7 @@ public class SYA_Voice : MonoBehaviourPun
 {
     public static SYA_Voice Instance;
     public Recorder recorder;
+    int currMicIdx;
 
     private void Awake()
     {
@@ -20,6 +21,23 @@ public class SYA_Voice : MonoBehaviourPun
         SYA_PlayerCompo.Instance.PlayerDestroy += OndestroyGo;
         
     }
+
+    /*private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            currMicIdx++;
+            currMicIdx %= Microphone.devices.Length;
+            recorder.PhotonMicrophoneDeviceId = currMicIdx;
+            DeviceInfo info = new DeviceInfo(currMicIdx, Microphone.devices[currMicIdx]);
+            recorder.MicrophoneDevice = info;
+            ////Recorder - Microphone Type 이 Unity or Photon인지에 따라 밑에 둘중 하나..
+            ////recorder.UnityMicrophoneDevice = Microphone.devices[currMicIdx];
+            //print(recorder.PhotonMicrophoneDeviceId + ", " + currMicIdx);
+            recorder.RestartRecording(true);
+        }
+
+    }*/
 
     public void OndestroyGo()
     {
